685f1a54c2e58ff10f9a4f368f705f92d8d1c95c
Ivan Arefkov <ivanarefkov@gmail.com>
2025-08-22 17:56:23 -0700

    :sparkles: add landing page

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-
+import { NuxtLayout } from '#components';
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-primary">
-      View Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
