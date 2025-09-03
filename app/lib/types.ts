592b2eca3a9d3580d5600d562e25a7a2a9a23263
Ivan Arefkov <ivanarefkov@gmail.com>
2025-09-02 20:00:17 -0700

    Location Names and Slugs are URL friendly and validated, queries are transfered to a new file

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,5 +1,6 @@
 // Types moved to /types/global.d.ts for global availability
 import type { User } from 'better-auth';
+import type { DrizzleError } from 'drizzle-orm';
 
 declare module 'h3' {
   // eslint-disable-next-line ts/consistent-type-definitions
@@ -8,4 +9,15 @@ declare module 'h3' {
       id: number;
     };
   }
+}
+
+// eslint-disable-next-line ts/consistent-type-definitions
+export interface CustomCauseError extends DrizzleError {
+  cause: {
+    cause: {
+      proto: {
+        message: string;
+      };
+    };
+  };
 }
--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,6 +1,10 @@
+import slugify from 'slug';
+
+import type { CustomCauseError } from '~/lib/types';
+
+import { checkSlug, existingLocationName } from '@/lib/db/queries/location';
 import db from '~/lib/db/index';
-import { InsertLocation } from '~/lib/db/schema';
-import { location } from '~/lib/db/schema/index';
+import { InsertLocation, location } from '~/lib/db/schema';
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -11,6 +15,7 @@ export default defineEventHandler(async (event) => {
   }
   const result = await readValidatedBody(event, InsertLocation.safeParse);
 
+  // If validation errors
   if (!result.success) {
     const statusMessage = result
       .error
@@ -32,10 +37,41 @@ export default defineEventHandler(async (event) => {
       data,
     }));
   }
-  const created = await db.insert(location).values({
-    ...result.data,
-    userId: event.context.user.id,
-    slug: result.data.name.replaceAll(' ', '-').toLocaleLowerCase(),
-  }).returning();
-  return created;
+
+  //  else  find a location check if it already exists for the user
+
+  const existingName = await existingLocationName(result.data, event.context.user.id);
+  if (existingName) {
+    return createError({
+      statusMessage: 'A location with this name already exists',
+      statusCode: 409,
+    });
+  }
+
+  const slug = await checkSlug(slugify(result.data.name));
+
+  try {
+    const [created] = await db.insert(location).values({
+      ...result.data,
+      userId: event.context.user.id,
+      slug,
+    }).returning();
+    return created;
+  }
+
+  catch (e) {
+    const error = e as CustomCauseError;
+    const errorMessage = error.cause.cause.proto.message;
+    if (errorMessage.includes('UNIQUE constraint failed:')) {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: 'Slug must be unique (Location name is used to generate slug)',
+      }));
+    }
+    throw errorMessage;
+    return createError({
+      statusCode: 500,
+      statusMessage: errorMessage,
+    });
+  }
 });
